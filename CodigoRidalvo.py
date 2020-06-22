7bf328268c2063966b1f678dbe3c173cdd2d509c
r1606 <65236136+r1606@users.noreply.github.com>
2020-06-21 21:45:22 -0300

    Update CodigoRidalvo.py

--- CodigoRidalvo.py
+++ CodigoRidalvo.py
@@ -3,27 +3,33 @@
 # Professor: Carlos Artur Guimarães
 # Aluno: Ridalvo Medeiros Alves de Oliveira
 #################################################
-**Gráfico por bairro**
-"""
+
+#################################################
+# Importações do NUNPY e do MATPOTLIB 
+#################################################
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Criar Título 
-plt.title('Ranking dos casos confirmados de Covid-19  por bairros de Natal ')
+#################################################
+# Carga do arquivo de dados ResMed.csv 
+#################################################
 
-
-
-# Crie um conjunto de dados
 height = [11.30, 8.22, 7.88, 6.51, 6.16, 5.48, 5.14, 4.79, 4.45, 4.45, 4.11, 3.42, 3.42, 3.42, 3.08, 2.74, 2.40, 1.71, 1.37, 1.37, 1.37, 1.37, 1.03, 1.03, 0.68, 0.68, 0.68, 0.68, 0.34, 0.34, 0.34, 0, 0, 0, 0, 0]
 bars = ('Potengi', 'Lagoa Nova', 'Nossa Senhora da Apresentação', 'Pajuçara', 'Tirol', 'Ponta Negra', 'Pitimbú', 'Lagoa Azul', 'Petrópolis', 'Capim Macio', 'Candelária', 'Quintas', 'Neópolis', 'Alecrim', 'Felipe Camarão', 'Barro Vermelho', 'Cidade Alta', 'Dix-Sepr Rosado', 'Planalto','Igapó', 'Cidade da Esperança','Bom Pastor', 'Nova Descoberta', 'Areia Preta', 'Rocas','Nordeste', 'Guarapes', 'Cidade Nova', 'Redinha', 'Nossa Senhora de Nazaré ', 'Lagoa Seca', 'Mãe Luiza', 'Praia do Meio', 'Ribeira', 'Salinas', 'Santos Reis')
 y_pos = np.arange(len(bars))
  
+#################################################
+# Formatação do gráfico 
+#################################################
+
+plt.title('Distribuição dos candidatos do processo seletivo para a Residência Médica') ### Título do gráfico
+plt.xticks(y_pos, bars) ### Título do eixo horizontal
+
+
 #Criar barras
 plt.bar(y_pos, height)
  
-# Crie nomes no eixo x
-plt.xticks(y_pos, bars)
  
 # Rotação dos nomes das barras
 plt.xticks(y_pos, bars, rotation=90)
