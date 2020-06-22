3e3887710cc62b19b0252d59840e4b393e0f89ce
Ridalvo Oliveira <65236136+r1606@users.noreply.github.com>
2020-06-22 00:15:53 -0300

    Update CodigoRidalvo.py

--- CodigoRidalvo.py
+++ CodigoRidalvo.py
@@ -20,80 +20,36 @@ import pandas as pd
 # df
 
 ### Programas da Residência Médica
-bars = ('101 - ANESTESIOLOGIA','102 - ANESTESIOLOGIA','103 - AREA CIRURGICA BÁSICA','104 - CARDIOLOGIA','105 - CIRURGIA DO APARELHO DIGESTIVO','106 - CIRURGIA GERAL','107 - CIRURGIA VIDEOLAPAROSCÓPICA','108 - CLÍNICA MÉDICA','109 - DERMATOLOGIA','110 - ENDOCRINOLOGIA E METEBOLOGIA','111 - ENDOCRINOLOGIA PEDIÁTRICA','112 - ENDOSCOPIA DIGESTIVA','113 - ENDOSCOPIA GINECOLÓGICA','114 - GASTROENTEROLOGIA','115 - GASTROENTEROLOGIA PEDIÁTRICA','116 - GINECOLOGIA E OBSTETRÍCIA','117 - GINECOLOGIA E OBSTETRÍCIA','118 - INFECTOLOGIA','119 - MEDICINA DE FAMÍLIA E COMUNIDADE','120 - MEDICINA DE FAMÍLIA E COMUNIDADE','121 - MEDICINA INTENSIVA','122 - NEFROLOGIA','123 - NEONATOLOGIA','124 - NEUROCIRURGIA','125 - NEUROLOGIA','126 - OFTALMOLOGIA','127 - OTORRINOLARINGOLOGIA','128 - PATOLOGIA','129 - PEDIATRIA','130 - PEDIATRIA','131 - PSIQUIATRIA','132 - RADIOLOGIA','133 - REUMATOLOGIA','134 - UROLOGIA')
+programas = ('101 - ANESTESIOLOGIA','102 - ANESTESIOLOGIA','103 - AREA CIRURGICA BÁSICA','104 - CARDIOLOGIA','105 - CIRURGIA DO APARELHO DIGESTIVO','106 - CIRURGIA GERAL','107 - CIRURGIA VIDEOLAPAROSCÓPICA','108 - CLÍNICA MÉDICA','109 - DERMATOLOGIA','110 - ENDOCRINOLOGIA E METEBOLOGIA','111 - ENDOCRINOLOGIA PEDIÁTRICA','112 - ENDOSCOPIA DIGESTIVA','113 - ENDOSCOPIA GINECOLÓGICA','114 - GASTROENTEROLOGIA','115 - GASTROENTEROLOGIA PEDIÁTRICA','116 - GINECOLOGIA E OBSTETRÍCIA','117 - GINECOLOGIA E OBSTETRÍCIA','118 - INFECTOLOGIA','119 - MEDICINA DE FAMÍLIA E COMUNIDADE','120 - MEDICINA DE FAMÍLIA E COMUNIDADE','121 - MEDICINA INTENSIVA','122 - NEFROLOGIA','123 - NEONATOLOGIA','124 - NEUROCIRURGIA','125 - NEUROLOGIA','126 - OFTALMOLOGIA','127 - OTORRINOLARINGOLOGIA','128 - PATOLOGIA','129 - PEDIATRIA','130 - PEDIATRIA','131 - PSIQUIATRIA','132 - RADIOLOGIA','133 - REUMATOLOGIA','134 - UROLOGIA')
 
 ### Quantidades de candidatos de cada programa
-height = [45,28,38,11,10,44,4,102,22,5,1,5,4,12,2,10,56,7,14,8,3,7,5,14,9,36,23,11,50,8,45,21,3,16] 
+qtcand = [45,28,38,11,10,44,4,102,22,5,1,5,4,12,2,10,56,7,14,8,3,7,5,14,9,36,23,11,50,8,45,21,3,16] 
 
-y_pos = np.arange(len(bars))
- 
 #################################################
 # Formatação do gráfico 
 #################################################
 
-plt.title('Distribuição dos candidatos do processo seletivo para a Residência Médica') ### Título do gráfico
-plt.xticks(y_pos, bars) ### Título do eixo horizontal
+### Título do gráfico
+plt.title('UFRN/COMPERVE \n Distribuição dos candidatos do processo seletivo para a Residência Médica 2020') 
 
+### Título do eixo vertical
+plt.ylabel('Quant. de candidatos')
 
-#Criar barras
-plt.bar(y_pos, height)
- 
- 
-# Rotação dos nomes das barras
-plt.xticks(y_pos, bars, rotation=90)
- 
-# Personalizar o layout da subtrama
-plt.subplots_adjust(bottom=0.4, top=0.99)
+### Título do eixo horizontal
+plt.xlabel('Programas') 
 
-#Cores 
-plt.bar(y_pos, height, color=[ 'red', 'green', 'red', 'red', 'red',  'blue', 'green', 'green', 'red', 'blue', 'green', 'green', 'yellow', 'green', 'blue', 'yellow', 'blue', 'blue', 'yellow', 'yellow', 'red', 'yellow', 'yellow', 'green', 'blue', 'blue', 'yellow', 'yellow', 'yellow', 'red', 'yellow', 'yellow', 'blue',  'blue',  'blue',  'blue', 'Red',  'blue' ])
-plt.xticks(y_pos, bars)
+# Monta a lista de programas no eixo horizontal
+pos_prog = np.arange(len(programas))
+plt.xticks(pos_prog, programas) 
+
+# Rotaciona os nomes das barras em 90 graus
+plt.xticks(pos_prog, programas, rotation=90)
+
+#Cria as barras do gráfico com as quantidades de candidatos, na sequência dos programas
+plt.bar(pos_prog, qtcand, width=0.3)
+  
+# Define a cor das barras como sendo verde
+plt.bar(pos_prog, qtcand, color=['green'])
+
+# Exibe o gráfico de barras
 plt.show()
-
-# Mostrar gráfico
-#plt.show()
-
-#"""**Gráfico Separado por Zona**"""
-
-# Importa biblioteca
-#import numpy as np
-#import matplotlib.pyplot as plt
- 
- # Titulo 
-#plt.title('Ranking dos casos confirmados de Covid-19  separado por zona')
-
-# Crie um conjunto de dados 
-#height = [32.19, 31.85, 21.22, 14.7]
-#bars = ('Zona Norte', 'Zona Sul', 'Zona Leste', 'Zona Oeste')
-#y_pos = np.arange(len(bars))
-
-# Separa as zonas por cores 
-#plt.bar(y_pos, height, color=['red', 'green', 'blue', 'yellow'])
-#plt.xticks(y_pos, bars)
-#plt.show()
-
-#"""**Os 5 bairros com mais casos confrimados de Covid-19**"""
-
-# Importa biblioteca
-#import numpy as np
-#import matplotlib.pyplot as plt
- 
- # Titulo 
-#plt.title('Ranking dos  5 bairros com mais casos confirmados de Covid-19' )
-
-
-# Crie um conjunto de dados 
-#height = [11.30, 8.22, 7.88, 6.51, 6.16]
-#bars = ('Potengi', 'Lagoa Nova', 'Nossa Senhora da Apresentação', 'Pajuçara', 'Tirol')
-#y_pos = np.arange(len(bars))
-
-# Rotação dos nomes das barras
-#plt.xticks(y_pos, bars, rotation=90)
- 
-# Personalizar o layout da subtrama
-#plt.subplots_adjust(bottom=0.4, top=0.99)
-
-# Separa as zonas por cores 
-#plt.bar(y_pos, height, color=['red', 'green', 'red', 'red','red'])
-#plt.xticks(y_pos, bars)
-#plt.show()
