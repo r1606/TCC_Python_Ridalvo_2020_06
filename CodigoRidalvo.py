7a5a8bf2b946edd1c9df4daa39173abc6000f6ba
Ridalvo Oliveira <65236136+r1606@users.noreply.github.com>
2020-06-21 22:55:30 -0300

    Update CodigoRidalvo.py

--- CodigoRidalvo.py
+++ CodigoRidalvo.py
@@ -19,9 +19,12 @@ import pandas as pd
 # df = pd.read_csv('ResMed.csv')
 # df
 
-
-height = [45,28,38,11,10,44,4,102,22,5,1,5,4,12,2,10,56,7,14,8,3,7,5,14,9,36,23,11,50,8,45,21,3,16]
+### Programas da Residência Médica
 bars = ('101 - ANESTESIOLOGIA','102 - ANESTESIOLOGIA','103 - AREA CIRURGICA BÁSICA','104 - CARDIOLOGIA','105 - CIRURGIA DO APARELHO DIGESTIVO','106 - CIRURGIA GERAL','107 - CIRURGIA VIDEOLAPAROSCÓPICA','108 - CLÍNICA MÉDICA','109 - DERMATOLOGIA','110 - ENDOCRINOLOGIA E METEBOLOGIA','111 - ENDOCRINOLOGIA PEDIÁTRICA','112 - ENDOSCOPIA DIGESTIVA','113 - ENDOSCOPIA GINECOLÓGICA','114 - GASTROENTEROLOGIA','115 - GASTROENTEROLOGIA PEDIÁTRICA','116 - GINECOLOGIA E OBSTETRÍCIA','117 - GINECOLOGIA E OBSTETRÍCIA','118 - INFECTOLOGIA','119 - MEDICINA DE FAMÍLIA E COMUNIDADE','120 - MEDICINA DE FAMÍLIA E COMUNIDADE','121 - MEDICINA INTENSIVA','122 - NEFROLOGIA','123 - NEONATOLOGIA','124 - NEUROCIRURGIA','125 - NEUROLOGIA','126 - OFTALMOLOGIA','127 - OTORRINOLARINGOLOGIA','128 - PATOLOGIA','129 - PEDIATRIA','130 - PEDIATRIA','131 - PSIQUIATRIA','132 - RADIOLOGIA','133 - REUMATOLOGIA','134 - UROLOGIA')
+
+### Quantidades de candidatos de cada programa
+height = [45,28,38,11,10,44,4,102,22,5,1,5,4,12,2,10,56,7,14,8,3,7,5,14,9,36,23,11,50,8,45,21,3,16] 
+
 y_pos = np.arange(len(bars))
  
 #################################################
@@ -46,9 +49,6 @@ plt.subplots_adjust(bottom=0.4, top=0.99)
 plt.bar(y_pos, height, color=[ 'red', 'green', 'red', 'red', 'red',  'blue', 'green', 'green', 'red', 'blue', 'green', 'green', 'yellow', 'green', 'blue', 'yellow', 'blue', 'blue', 'yellow', 'yellow', 'red', 'yellow', 'yellow', 'green', 'blue', 'blue', 'yellow', 'yellow', 'yellow', 'red', 'yellow', 'yellow', 'blue',  'blue',  'blue',  'blue', 'Red',  'blue' ])
 plt.xticks(y_pos, bars)
 plt.show()
-
-
-
 
 # Mostrar gráfico
 #plt.show()
